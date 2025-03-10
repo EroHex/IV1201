3e012813d2d91c0d94b2ebe1b8862852dcd25182
EliazBid <eliaz@kth.se>
2025-03-10 16:22:31 +0100

    Added pages to manage-applications to hopefully speed up loading
    Co-Authored-By: Mikael <137923338+MickeR94@users.noreply.github.com>

--- Recruitment/src/main/java/project/com/Recruitment/repository/PersonRepository.java
+++ Recruitment/src/main/java/project/com/Recruitment/repository/PersonRepository.java
@@ -1,9 +1,12 @@
 package project.com.Recruitment.repository;
 
-import org.springframework.data.jpa.repository.EntityGraph;
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import project.com.Recruitment.model.Person;
-import java.util.*;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
     
@@ -19,7 +22,9 @@ public interface PersonRepository extends JpaRepository<Person, Long> {
      * @param roleId   1 or 2, 1 = recruiter, 2 = applicants
      * @return  List of persons with param role_id
      */
-    List<Person> findByRoleId(Long roleId);
+    //List<Person> findByRoleId(Long roleId);
+    //OBS! Testar att använda Page istället för List
+    Page<Person> findByRoleId(Long roleId, Pageable pageable);
 
     /**
      * Finds and returns a person object for specific application info
